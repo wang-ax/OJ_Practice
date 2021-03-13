ab007d824a1083e33407eb339b4934f7040c052d
wang-ax <3071254175@qq.com>
2021-03-13 22:17:42 +0800

    句子逆序（将句子逆序，但是单词还是要正确）

---
+++ 每日一题/句子逆序/Main.java
@@ -0,0 +1,49 @@
+package 句子逆序;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * ClassName 句子逆序
+ * Description TODO
+ * Author 30712
+ * Date 2021-03-13
+ * Time 8:46
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            String s =scanner.nextLine();
+            System.out.println(reverseSentence(s));
+        }
+    }
+
+    private static String reverseSentence(String s) {
+        //
+        StringBuilder stringBuilder = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+        for (int i = chars.length-1;i>=0;i--){
+
+            //遇到空格就将前面的元素全部出栈
+            if (chars[i] == ' ' ){
+                while (!stack.isEmpty()  ){
+                    char c  = stack.pop();
+                    stringBuilder.append(c);
+                }
+                stringBuilder.append(chars[i]);
+            }else {
+                stack.push(chars[i]);
+            }
+
+            if (i == 0){
+                while (!stack.isEmpty()  ){
+                    char c  = stack.pop();
+                    stringBuilder.append(c);
+                }
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
