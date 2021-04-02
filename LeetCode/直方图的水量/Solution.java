8309867e7900cf5a015e30a72959fe845b502226
wang-ax <3071254175@qq.com>
2021-04-02 23:16:03 +0800

    直方图的水量

---
+++ LeetCode/直方图的水量/Solution.java
@@ -0,0 +1,14 @@
+package 直方图的水量;
+
+/**
+ * ClassName 直方图的水量
+ * Description TODO
+ * Author 30712
+ * Date 2021-04-02
+ * Time 22:58
+ */
+public class Solution {
+    public int trap(int[] height) {
+
+    }
+}
