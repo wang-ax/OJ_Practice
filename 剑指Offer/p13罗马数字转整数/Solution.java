1237ccb09027e0817649a2c1326ea5139ac6be11
wang-ax <3071254175@qq.com>
2021-06-04 18:15:05 +0800

    罗马数字转换成整数

---
+++ 剑指Offer/p13罗马数字转整数/Solution.java
@@ -0,0 +1,44 @@
+package p13罗马数字转整数;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * ClassName p_13罗马数字转整数
+ * Description TODO
+ * Author 30712
+ * Date 2021-06-04
+ * Time 17:42
+ */
+class Solution {
+    public  static int romanToInt(String s) {
+        Map<Character, Integer> symbolValues = new HashMap<Character, Integer>() {{
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
+        }};
+        int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            int value = symbolValues.get(s.charAt(i));
+            //前一个数字如果比后一个数字小，就说明该数是由两个罗马数字组成的
+            //减去小的加上大的，最终的结果不发生变化
+            if (i < n - 1 && value < symbolValues.get(s.charAt(i + 1))) {
+                ans -= value;
+            } else {
+                ans += value;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String s = "MCMXCIV";
+        System.out.println(romanToInt(s));
+
+    }
+}
