5f5f40220fa078d811f5f150100d4db32923c2a4
wang-ax <3071254175@qq.com>
2021-06-19 18:15:42 +0800

    回溯——暴力解法

--- 算法/回溯/chuanlianzifuchuanmaxLength.java
+++ 算法/回溯/chuanlianzifuchuanmaxLength.java
@@ -1,5 +1,7 @@
 package p_0619;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -10,7 +12,7 @@ import java.util.List;
  * Time 15:35
  */
 public class chuanlianzifuchuanmaxLength {
-    private int max; //记录当前的最长的长度
+    /*private int max; //记录当前的最长的长度
     public int maxLength(List<String> arr) {
         if(arr == null){
             return 0;
@@ -19,12 +21,12 @@ public class chuanlianzifuchuanmaxLength {
         return max;
     }
 
-    /**
+    *//**
      * 先判断要传进来的sb是不是满足每一个字符都只出现一次
      * @param arr
      * @param start
      * @param sb
-     */
+     *//*
     private void dfs(List<String> arr, int start, StringBuilder sb) {
         if(isRepeat(sb)){
             return;
@@ -38,11 +40,11 @@ public class chuanlianzifuchuanmaxLength {
         }
     }
 
-    /**
+    *//**
      * 判断sb中是不是每一个元素都只出现了一次
      * @param sb
      * @return
-     */
+     *//*
     private boolean isRepeat(StringBuilder sb) {
         int[] count = new int[26];
         for (int i=0;i< sb.length();i++){
@@ -53,5 +55,41 @@ public class chuanlianzifuchuanmaxLength {
             }
         }
         return false;
+    }*/
+
+    int maxLength = 0;
+    public int maxLength(List<String> arr) {
+        ArrayList<String> strs = new ArrayList<>();
+        dfs(arr, 0, strs);
+        return maxLength;
+    }
+    public void dfs(List<String> arr, int startIndex, ArrayList<String> strs) {
+        if (check(strs)){
+            int tempLen = 0;
+            for (String str : strs) {
+                tempLen += str.length();
+            }
+            if (tempLen > maxLength){
+                maxLength = tempLen;
+            }
+        }
+
+        for (int i = startIndex; i < arr.size(); i++){
+            strs.add(arr.get(i));
+            dfs(arr, i+1, strs);
+            strs.remove(strs.size()-1);
+        }
+    }
+    public boolean check(ArrayList<String> strs) {
+        HashMap<Character, Boolean> map = new HashMap<>();
+        for (String str : strs) {
+            for (int i = 0; i < str.length(); i++){
+                if (map.get(str.charAt(i)) != null && map.get(str.charAt(i)) == true){
+                    return false;
+                }
+                map.put(str.charAt(i), true);
+            }
+        }
+        return true;
     }
 }
