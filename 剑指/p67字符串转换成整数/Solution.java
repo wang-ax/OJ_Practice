b5c8ad11a010b6d704210efd69c3a1647b5d3d81
wang-ax <3071254175@qq.com>
2021-07-03 16:15:56 +0800

    字符串转换成整数

--- 剑指/p67字符串转换成整数/Solution.java
+++ 剑指/p67字符串转换成整数/Solution.java
@@ -45,4 +45,29 @@ public class Solution {
         //首位是除了正负号和合法的整数之外的其他字符，直接返回
         return 0;
     }
+    public int strToInt2(String str){
+        char[] chars = str.trim().toCharArray();
+        if (chars.length == 0){
+            return 0;
+        }
+        int res=0;
+        int i=1;
+        int bndry = Integer.MAX_VALUE/10;//大数边界
+        int sign =1;
+        if (chars[0] == '-'){
+            sign =-1;
+        }else if(chars[0] != '+'){
+            i=0;
+        }
+        for (int j=i;j < chars.length;j++){
+            if (chars[j] < '0' || chars[j] >'9'){
+                break;
+            }
+            if (res > bndry || res == bndry && chars[j] > '7'){
+                return sign ==1 ? Integer.MAX_VALUE:Integer.MIN_VALUE;
+            }
+            res = res*10 + (chars[j] - '0');
+        }
+        return sign *res;
+    }
 }
