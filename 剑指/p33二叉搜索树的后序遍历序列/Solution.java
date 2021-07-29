e42616a8f02516398455b49b1d8466026c739171
wang-ax <3071254175@qq.com>
2021-07-29 10:42:30 +0800

    判断数组是不是二叉搜索树的后序遍历的序列 https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-hou-xu-bian-li-xu-lie-lcof/

--- 剑指/p33二叉搜索树的后序遍历序列/Solution.java
+++ 剑指/p33二叉搜索树的后序遍历序列/Solution.java
@@ -14,16 +14,18 @@ package p33二叉搜索树的后序遍历序列;
  * 数组的最后一个即为二叉树的根节点
  */
 class Solution{
-    public boolean verifySquenceOfBST(int[] sequence) {
+    public static boolean verifyPostorder(int[] sequence) {
         if (sequence == null || sequence.length <= 0)
             return true;
         return verifyCore(sequence, 0, sequence.length - 1);
     }
-    private boolean verifyCore(int[] sequence, int start, int end) {
-        if (start >= end){
+    public static boolean verifyCore(int[] sequence, int start, int end) {
+        if (start >= end){//该子树的节点数量<=1 ,直接返回true
             return true;
         }
         //判断左子树
+        //从左网友找出第一个比根结点大的值，它前面的都是根结点的左子树的节点
+        //它后面的都是根结点的右子树的节点（包含当前值，不包含最后一个值，因为最后一个是根结点）
         int mid = start;
         while (sequence[mid] < sequence[end]){
             mid++;
@@ -34,9 +36,15 @@ class Solution{
             if (sequence[i] < sequence[end])
                 return false;
         }
+
         //判断左子树和右子树是否满足
         return verifyCore(sequence, start, mid - 1) &&
                 verifyCore(sequence, mid, end - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,2,6,5};
+        System.out.println(verifyPostorder(arr));
     }
 }
 
