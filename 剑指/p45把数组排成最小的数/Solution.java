9c311aa3ba33042bb7ca56c43eff7ecf7a4c8522
wang-ax <65639372+wang-ax@users.noreply.github.com>
2021-08-18 23:36:46 +0800

    把数组排成最小的数
    贪心-字符串-排序

---
+++ 剑指/p45把数组排成最小的数/Solution.java
@@ -0,0 +1,14 @@
+package p45把数组排成最小的数;
+
+/**
+ * ClassName p45把数组排成最小的数
+ * Description TODO
+ * Author 30712
+ * Date 2021-08-18
+ * Time 23:34
+ */
+public class Solution {
+    public String minNumber(int[] nums){
+
+    }
+}
