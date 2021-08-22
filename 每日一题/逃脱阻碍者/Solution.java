711455f07c70d2fb44c36d63c9624e48a37c230a
wang-ax <65639372+wang-ax@users.noreply.github.com>
2021-08-22 23:50:27 +0800

    逃脱阻碍者

---
+++ 每日一题/逃脱阻碍者/Solution.java
@@ -0,0 +1,23 @@
+package p08.p_0822.逃脱阻碍者;
+
+/**
+ * ClassName p08.p_0822.逃脱阻碍者
+ * Description TODO
+ * Author 30712
+ * Date 2021-08-22
+ * Time 21:12
+ */
+public class Solution {
+    /**
+     *
+     * @param ghosts 阻碍者的位置,第i个阻碍者从ghosts[xi,yi]出发
+     * @param target 目的地的位置
+     *        从[0,0]点开始出发
+     * @return
+     * 玩家按照最短的路径向目的地移动
+     * 阻碍者
+     */
+    public boolean escapeGhosts(int[][] ghosts, int[] target) {
+
+    }
+}
