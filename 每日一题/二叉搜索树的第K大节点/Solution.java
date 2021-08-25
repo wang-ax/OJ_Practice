8db8ab9a0714b21a47218b2e3aaa09335fd7af4e
wang-ax <65639372+wang-ax@users.noreply.github.com>
2021-08-25 23:44:51 +0800

    二叉搜索树的第K大节点

---
+++ 每日一题/二叉搜索树的第K大节点/Solution.java
@@ -0,0 +1,29 @@
+package p06.p_0609.二叉搜索树的第K大节点;
+
+
+/**
+ * ClassName p06.p_0609.二叉搜索树的第K大节点
+ * Description TODO
+ * Author 30712
+ * Date 2021-06-09
+ * Time 11:21
+ */
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Solution {
+//    public int kthLargest(TreeNode root, int k) {
+//        //二叉搜索树的第K大个节点
+//        //二叉搜索树的中序遍历是有序的
+//        //求中序遍历的倒数第K个节点
+//
+//    }
+
+}
